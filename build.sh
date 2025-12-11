22927ba75f53002920229d4770bac1e43399ef83
chenxc <997909544@qq.com>
2025-12-11 16:49:26 +0800

    fix: build

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
-CARGO_HOME="${CARGO_HOME:-$HOME/.cargo}"
-echo "Using CARGO_HOME=$CARGO_HOME"
+mkdir -vp ${CARGO_HOME:-$HOME/.cargo}
 
-mkdir -p "$CARGO_HOME"
-
-cat <<EOF > "$CARGO_HOME/config.toml"
+cat << EOF | tee -a ${CARGO_HOME:-$HOME/.cargo}/config.toml
 [source.crates-io]
 replace-with = 'mirror'
 
 [source.mirror]
 registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
 EOF
-
-cargo build --release
